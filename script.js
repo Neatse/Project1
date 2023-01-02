7e78af90ccaa8dc81d358a7fdaa95f266cc6cc44
Tony <113856232+Neatse@users.noreply.github.com>
2023-01-02 18:39:17 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-var topBlock = document.querySelector("#top-block");
-var sideBlock = document.querySelector("#side-block");
 var mainBlock = document.querySelector("#main-block");
 
 var topTitle = document.querySelector('#top-block h5');
@@ -15,17 +13,14 @@ var jupiterName = document.querySelector('#jupiter');
 var saturnName = document.querySelector('#saturn');
 var uranusName = document.querySelector('#uranus');
 var neptuneName = document.querySelector('#neptune');
-var backButton = document.querySelector('#previous');
 
 var API_KEY = "Z9G4UcTMnsu5PmaUes8zvAlCRB52JooGMkanIgoD";
+var NUM_PICTURES = 2;
 
 var pictureOfTheDayURL = "https://api.nasa.gov/planetary/apod?api_key="; // + API_KEY
 var imageSearchURL = "https://images-api.nasa.gov/search?q="; // + query
 
-// The date being shown
-var currentDailyPictureDate = new Date(); // Subtract one day and save each time
-
-// Gets the data from the API, then passes it to the "onDone" function
+// Gets the data from the API, then passes it to the data handler function
 function fetchDPData() {
     // Promise style
     fetch(pictureOfTheDayURL + API_KEY) // + "&date=" + {Date in YYYY-MM-DD format}
@@ -63,12 +58,12 @@ function handleNASAImageData(data, objectName) {
     // Clear the previous elements
     mainBlock.innerHTML = `<h3>${objectName}</h3>`;
 
-    for (var i = 0; i < 1; i++) {
+    for (var i = 0; i < NUM_PICTURES; i++) {
         // Get the next Item:
         // var currentItem = items[i];
 
         // Get the random Item:
-        var randomIndex = Math.floor(Math.random() * length); // 0 -> length;
+        var randomIndex = Math.floor(Math.random() * length); // 0 -> length; // Math.ceil(#) - rounds up
         var currentItem = items[randomIndex];
 
         // Get the data we want:
@@ -92,48 +87,64 @@ function handleNASAImageData(data, objectName) {
     }
 }
 
-// Calls the API with the handler for the first time
+// Calls the APIs with the handler for the first time
 fetchDPData();
 fetchNASAData('Earth');
 
 // Adds click handlers for every planet name
 mercuryName.addEventListener('click', () => {
     fetchNASAData('Mercury');
+    document.querySelector('.current').className = '';
+    mercuryName.className = 'current';
     console.log('Clicked Mercury');
 });
 
 venusName.addEventListener('click', () => {
     fetchNASAData('Venus');
+    document.querySelector('.current').className = '';
+    venusName.className = 'current';
     console.log('Clicked Venus');
 });
 
 earthName.addEventListener('click', () => {
     fetchNASAData('Earth');
+    document.querySelector('.current').className = '';
+    earthName.className = 'current';
     console.log('Clicked Earth');
 });
 
 marsName.addEventListener('click', () => {
     fetchNASAData('Mars');
+    document.querySelector('.current').className = '';
+    marsName.className = 'current';
     console.log('Clicked Mars');
 });
 
 jupiterName.addEventListener('click', () => {
     fetchNASAData('Jupiter');
+    document.querySelector('.current').className = '';
+    jupiterName.className = 'current';
     console.log('Clicked Jupiter');
 });
 
 saturnName.addEventListener('click', () => {
     fetchNASAData('Saturn');
+    document.querySelector('.current').className = '';
+    saturnName.className = 'current';
     console.log('clicked Saturn');
 });
 
 uranusName.addEventListener('click', () => {
     fetchNASAData('Uranus');
+    document.querySelector('.current').className = '';
+    uranusName.className = 'current';
     console.log('clicked Uranus');
 });
 
 neptuneName.addEventListener('click', () => {
     fetchNASAData('Neptune');
+    document.querySelector('.current').className = '';
+    neptuneName.className = 'current';
     console.log('clicked Neptune');
 });
 
